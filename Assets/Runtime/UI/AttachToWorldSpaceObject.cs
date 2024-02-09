5a6554fd19fe2b71d2125bf49f48fbf8dd98f5a9
Travis R. French <travis.french.1991@gmail.com>
2024-02-09 14:01:33 -0800

    [BUGFIX] COLOR-103 Fixed healthbars out of sync with the attached entity (#5)
    * Started rebuiling UI architecture for enemy health.
    
    * Fixed enemy health bar jittering and some sort order issues with the player's health bar.

---
+++ Assets/Runtime/UI/AttachToWorldSpaceObject.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.UI
+{
+    /// <summary>
+    /// Attaches to a world space object and maintains the specified offset from it in screen space.
+    /// </summary>
+    public class AttachToWorldSpaceObject : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform objectToFollow;
+        [SerializeField]
+        private Vector2 offset;
+        private RectTransform thisTransform;
+        private Camera mainCamera;
+
+        /// <summary>
+        /// The world space object to which this object should attach.
+        /// </summary>
+        public Transform ObjectToFollow
+        {
+            get => this.objectToFollow;
+            set => this.objectToFollow = value;
+        }
+        /// <summary>
+        /// The offset to retain while attached.
+        /// </summary>
+        public Vector2 Offset
+        {
+            get => this.offset;
+            set => this.offset = value;
+        }
+
+        private void Awake()
+        {
+            this.thisTransform = this.GetComponent<RectTransform>();
+            this.mainCamera = Camera.main;
+        }
+        private void Update()
+        {
+            if (this.ObjectToFollow == null || this.mainCamera == null)
+            {
+                return;
+            }
+            
+            var screenPoint = this.mainCamera.WorldToScreenPoint(this.ObjectToFollow.position);
+            this.thisTransform.position = screenPoint + (Vector3)this.Offset;
+        }
+    }
+}
