0ae32c5293bf2be8fff059d4b0d7905cc24ff1b8
Travis French <travis.french.1991@gmail.com>
2024-02-10 17:50:05 -0800

    More housekeeping changes.

--- Assets/Runtime/Conditions/AOECondition.cs
+++ Assets/Runtime/Conditions/AOECondition.cs
@@ -10,61 +10,6 @@ namespace ScringloGames.ColorClash.Runtime.Conditions
         private readonly float radius = 2f;
         private readonly int damage;
         private readonly Func<GameObject, bool> filter;
-        // private bool hasCondition;
-        
-        // public override void OnApplied(ConditionBank bank)
-        // {
-        //     Debug.Log($"applied condition onto: {bank.name}");
-        //     // Sets the Duration of this condition to 2 seconds.
-        //     bank.GetComponent<HealthHandler>().TakeDamage(15);
-        //
-        //     // Gets the position of the object that this ConditionBank is on
-        //     var thisPos = bank.transform.position;
-        //
-        //     // Stores an array of Colliders within a radius of this object.
-        //     var colliders = Physics2D.OverlapCircleAll(thisPos, this.radius);
-        //
-        //     // Foreach loop goes through each collider found, checks for if each object has a ConditionBank component, if it does,
-        //     //then it stores that bank in a variable, and then checks the list of conditions that the ConditionBank has.
-        //
-        //     foreach (var target in colliders)
-        //     {
-        //         if (target.GetComponent<ConditionBank>() != null)
-        //         {
-        //             this.hasCondition = false;
-        //             var targetBank = target.GetComponent<ConditionBank>();
-        //             foreach (var targetCond in targetBank.Conditions)
-        //             {
-        //                 // If any of the conditions on the current ConditionBank are the AOECondtion, it will skip this object
-        //                 //and start checking the next object in the list.
-        //                 if (targetCond is AOECondition)
-        //                 {
-        //                     this.hasCondition = true;
-        //                 }
-        //             } 
-        //             // If none of the conditions on the current ConditionBank are the AOECondition, this foreach loop will apply
-        //             // all of the conditions on THIS object onto the ConditionBank of the next object. 
-        //             if (!this.hasCondition)
-        //             {
-        //                 foreach (var thisCond in bank.Conditions)
-        //                 {
-        //                     // Case uses pattern casting!
-        //                     Condition newCondition = thisCond switch
-        //                     {
-        //                         SlowCondition condition => new SlowCondition(
-        //                             condition.SlowPercent,
-        //                             condition.Duration - condition.Time),
-        //                         DOTCondition condition => new DOTCondition(
-        //                             condition.DamagePerTick,
-        //                             condition.Duration - condition.Time),
-        //                         _ => null
-        //                     };
-        //                     if (newCondition != null) {targetBank.Apply(newCondition); };
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
 
         public AOECondition(float duration, float radius, int damage, Func<GameObject, bool> filter)
             : base(duration)
