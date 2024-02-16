b2b0cddd7a6b7cac23d182f8001dea265ac46591
Travis French <travis.french.1991@gmail.com>
2024-02-16 15:19:18 -0800

    Continued rebuild and created intern enemy with basic movement.

---
+++ Assets/Runtime/AI/InternAIBrain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ScringloGames.ColorClash.Runtime.Movement;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.AI
+{
+    public class InternAIBrain : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject target;
+        [SerializeField]
+        private DestinationMover mover;
+
+        private void OnEnable()
+        {
+            this.target = GameObject.FindWithTag("Player");
+        }
+
+        private void Update()
+        {
+            var destination = this.target.transform.position;
+            var distance = Vector2.Distance(this.transform.position, destination);
+            
+            if (distance > 0f)
+            {
+                this.mover.MoveTo(destination);
+            }
+        }
+    }
+}
