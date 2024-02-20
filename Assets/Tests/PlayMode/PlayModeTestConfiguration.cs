568bb599ffe518a1c211147ee84d7d5db2799afa
Travis R. French <travis.french.1991@gmail.com>
2024-02-19 16:44:03 -0800

    [BUGFIX] COLOR-117 Clamped health between zero and max health in heal method (#20)
    * Added tests assembly.
    
    * Clamped health between zero and max health.

---
+++ Assets/Tests/PlayMode/PlayModeTestConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ScringloGames.ColorClash.Runtime;
+using ScringloGames.ColorClash.Runtime.GameServices;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Tests.PlayMode
+{
+    [CreateAssetMenu(menuName = "Scriptables/Test Configurations/Play Mode")]
+    public class PlayModeTestConfiguration : ScriptableObject
+    {
+        public RegistrarSet Registrars;
+        public List<GameService> Services;
+        public GameObject enemyPrefab;
+
+        public GameObject EnemyPrefab => this.enemyPrefab;
+
+        public void Setup()
+        {
+            this.Registrars.Setup();
+
+            foreach (var service in this.Services)
+            {
+                service.Setup();
+            }
+        }
+
+        public void Teardown()
+        {
+            foreach (var service in this.Services)
+            {
+                service.Teardown();
+            }
+        }
+    }
+}
