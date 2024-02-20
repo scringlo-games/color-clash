279bb9aa65803e37f49881d6673bf946c8eb1a49
kyoninh <132638948+kyoninh@users.noreply.github.com>
2024-02-19 16:01:05 -0800

    added damage number prefab + ShowDamageOnHit script

---
+++ Assets/Runtime/UI/DamageNumberSetup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageNumberSetup : MonoBehaviour
+{
+  private bool active;
+  private TextMeshPro text;
+  [SerializeField]
+  float aliveTimeMax;
+  float aliveTime;
+  Color thisColor;
+  float colorAlpha;
+  float posY;
+  public void Setup(float damage, Color color)
+  {
+    text = GetComponent<TextMeshPro>();
+    if(damage>= 0)//damaged
+    {
+      text.SetText($"-{MathF.Abs(damage).ToString()}");
+    }
+    if(damage<=0)//healed
+    {
+      text.SetText($"+{MathF.Abs(damage).ToString()}");
+    }
+    text.color = color;
+    thisColor = color;
+    active = true;
+    aliveTime = aliveTimeMax;
+    colorAlpha = text.color.a;
+    posY = transform.position.y;
+  }
+  void Update()
+  {
+    if(active)
+    {
+      aliveTime -= Time.deltaTime; 
+      float t = aliveTime/aliveTimeMax;
+      //changes the alpha value of the gradually over the number's lifetime. 
+      text.color = new Color(thisColor.r,thisColor.g,thisColor.b,Mathf.Lerp(colorAlpha/4, colorAlpha, t));
+      float thisScale = Mathf.Lerp(1.5f,1f,t);
+      transform.localScale = new Vector2(thisScale,thisScale);
+      transform.position = new Vector2(transform.position.x, Mathf.Lerp(posY +1f, posY, t));
+      if(aliveTime <= 0)
+      {
+        Destroy(this.gameObject);
+      }
+
+    }
+  }
+  
+}
