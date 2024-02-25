b1a3a6a12d5ca434e3a23499ad52f00b4178fc94
SirKingII <byler842@gmail.com>
2024-02-25 15:26:50 -0800

    Looks at player
    He look!!

--- Assets/Runtime/AI/PencilPusherAIBrain.cs
+++ Assets/Runtime/AI/PencilPusherAIBrain.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ScringloGames.ColorClash.Runtime.Aiming;
+using ScringloGames.ColorClash.Runtime.Weapons;
 using UnityEngine;
 
 public class PencilPusherAIBrain : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+     * The Plan:
+     * Constantly aims at player
+     * Constantly shooting at player
+     * Cooldown comes from PaintLauncher script
+     * Maybe moves? Probably not?
+     */
+
+    private GameObject target;
+    [SerializeField] private DirectionalLooker looker;
+    [SerializeField] private ProjectileLauncher projectileLauncher;
+    void OnEnable()
     {
-        
+        target = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        var targetPosition = this.target.transform.position;
+        var direction = (targetPosition - this.transform.position).normalized;
+        looker.Direction = direction;
         
     }
 }
