34c4a1c51f2ce43eb1669164e4dd88bf646a7da8
SirKingII <144304493+SirKingII@users.noreply.github.com>
2024-02-25 18:11:51 -0800

    [FEATURE] COLOR-67 Made projectiles self destruct when they stop moving (#35)
    * Made the wrong script!
    
    Whoops!
    
    * Projectiles now destroy around when they stop!

---
+++ Assets/DestroysSelfOnTimer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScringloGames.ColorClash.Runtime.Shared;
+using UnityEngine;
+
+public class DestroysSelfOnTimer : MonoBehaviour
+{
+    [SerializeField] private float Lifetime;
+    [SerializeField] private Destructible destructible;
+
+    private float lifeLeft;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        lifeLeft = Lifetime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifeLeft <= 0)
+        {
+            destructible.Destroy();
+        }
+        else
+        {
+            lifeLeft -= Time.deltaTime;
+        }
+    }
+}
