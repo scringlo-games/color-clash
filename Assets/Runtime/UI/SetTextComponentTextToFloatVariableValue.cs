65e3b346f5061053b736c300a5d2b69cacc4de5a
Travis French <travis.french.1991@gmail.com>
2024-02-29 04:09:18 -0800

    Prototyped a coverage summary screen.

---
+++ Assets/Runtime/UI/SetTextComponentTextToFloatVariableValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ScringloGames.ColorClash.Runtime.Shared.ScriptableVariables;
+using TMPro;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.UI
+{
+    [ExecuteAlways]
+    public class SetTextComponentTextToFloatVariableValue : MonoBehaviour
+    {
+        [TextArea]
+        [SerializeField]
+        private string formatString = "{0}";
+        [Header("Dependencies")]
+        [SerializeField]
+        private TextMeshProUGUI textMeshComponent;
+        [SerializeField]
+        private FloatVariable floatVariable;
+
+        private void Update()
+        {
+            if (this.textMeshComponent.text == null)
+            {
+                return;
+            }
+
+            if (this.floatVariable == null)
+            {
+                return;
+            }
+
+            this.textMeshComponent.text = string.Format(this.formatString, this.floatVariable.Value);
+        }
+    }
+}
---
+++ Assets/Runtime/UI/Windows/Window.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.UI.Windows
+{
+    [ExecuteAlways]
+    public class Window : MonoBehaviour
+    {
+        [SerializeField]
+        private string title = "Window";
+        [Header("Dependencies")]
+        [SerializeField]
+        private TextMeshProUGUI titleTextComponent;
+
+        public string Title
+        {
+            get => this.title;
+            set => this.title = value;
+        }
+        
+        private void Update()
+        {
+            if (this.titleTextComponent == null)
+            {
+                return;
+            }
+            
+            this.titleTextComponent.text = this.title;
+        }
+    }
+}
