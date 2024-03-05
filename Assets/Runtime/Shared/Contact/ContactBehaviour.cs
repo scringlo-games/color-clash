7755940f3fafc8652d6fcf524245ca0965eee450
SirKingII <144304493+SirKingII@users.noreply.github.com>
2024-03-05 13:51:04 -0800

    [BUGFIX] COLOR-132 Healing paint does not heal when initially applying damage (#44)
    * Fixed a bug where healing was not being applied to projectiles correctly.
    
    * updated projectile prefabs with healing to makethem more reasonable
    
    * tweaked damage number prefab
    
    * Fixed a bug where melee weapons did no damage.
    
    * modified damage number script to round damage/healing
    
    * Minor balance tweak
    
    Blue Proj. Heal Percent .3 -> .2
    Other than that, everything feels great at .1!
    This makes me want to do the proposed rework where monocolor mixes apply 2 stacks instead of 1 stronger stack...
    
    ---------
    
    Co-authored-by: Travis French <travis.french.1991@gmail.com>
    Co-authored-by: kyoninh <132638948+kyoninh@users.noreply.github.com>

---
+++ Assets/Runtime/Shared/Contact/ContactBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.Shared.Contact
+{
+    [RequireComponent(typeof(ContactHandler))]
+    public abstract class ContactBehaviour : MonoBehaviour
+    {
+        public virtual void OnCollisionEntered(Collision2D collision2D)
+        {
+        }
+        
+        public virtual void OnCollisionStayed(Collision2D collision2D)
+        {
+        }
+        
+        public virtual void OnCollisionExited(Collision2D collision2D)
+        {
+        }
+        
+        public virtual void OnTriggerEntered(Collider2D collider2D)
+        {
+        }
+        
+        public virtual void OnTriggerStayed(Collider2D collider2D)
+        {
+        }
+        
+        public virtual void OnTriggerExited(Collider2D collider2D)
+        {
+        }
+    }
+}
