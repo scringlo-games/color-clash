a8b1479baf406db5232bdccf29fe41355124ad77
Travis R. French <travis.french.1991@gmail.com>
2024-03-06 03:10:49 -0800

    [RELEASE] v0.5.0 (#47)
    * Installed AStar Pathfinding Project plugin.
    
    * Built a simple finite state machine.
    
    * Rebuilt movement systems to use the new pathfinding plugin.
    
    * Reworked intern brain to use new state machine and pathing.
    
    * Added a decal coverage tracking system.
    
    * Updated PaintCoverageTracker to use a ScriptableVariable.
    
    * Prototyped a coverage summary screen.
    
    * Continued prototype of summary screen.
    
    * Made summary screen show when the room is cleared.
    
    * Rebuilt supporting systems for better cohesion with summary screen.
    
    * Adjusted scene service.
    
    * Housekeeping.
    
    * Work in progress.
    
    * Rebuilt enemy AI and other relevant settings to use the new movement system.
    
    * updated the size of the foam fist sprite
    
    * Continued implementing summary stats.
    
    * Removed generated visual scripting files.
    
    * Updated .gitignore to exclude transient visual scripting files.
    
    * Updated .gitignore to ignore visual scripting directory meta file.
    
    * Increased size of the boss hitbox.
    
    * Adjusted boss scene to better fit the now-fixed weapon reload timer.
    
    * Updated summary screen.
    
    * Fixed various integration bugs.
    
    ---------
    
    Co-authored-by: kyoninh <132638948+kyoninh@users.noreply.github.com>

---
+++ Assets/Runtime/AI/SeekTargetBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ScringloGames.ColorClash.Runtime.Movement;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime.AI
+{
+    public class SeekTargetBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private float minimumDistance = 0.05f;
+        [SerializeField]
+        private float maximumDistance = Mathf.Infinity;
+        [SerializeField]
+        private float deltaDistance = 0.5f;
+        [Header("Dependencies")]
+        [SerializeField]
+        private AStarDestinationMover mover;
+        private GameObject target;
+
+        private void OnEnable()
+        {
+            this.target = GameObject.FindWithTag("Player");
+            this.mover.MoveTo(this.target.transform.position);
+        }
+
+        private void OnDisable()
+        {
+            this.mover.Halt();
+        }
+
+        private void Update()
+        {
+            if (this.target == null)
+            {
+                return;
+            }
+            
+            var from = this.mover.transform.position;
+            var to = this.target.transform.position;
+            var distanceToTarget = Vector2.Distance(from, to);
+
+            if (distanceToTarget <= this.minimumDistance || distanceToTarget >= this.maximumDistance)
+            {
+                return;
+            }
+
+            if (Vector2.Distance(this.mover.Destination, to) > this.deltaDistance)
+            {
+                this.mover.MoveTo(to);
+            }
+        }
+    }
+}
