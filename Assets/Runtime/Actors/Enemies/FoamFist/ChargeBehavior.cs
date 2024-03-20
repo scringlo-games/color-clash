ca6d1ff554a6e06d2869f04b5946eaf8479828b4
Travis R. French <travis.french.1991@gmail.com>
2024-03-19 21:21:48 -0700

    [FEATURE] COLOR-148 Implemented foam fist charge attack (#55)
    * modified foam fist prefab
    
    * updated foam fist prefab
    
    ---------
    
    Co-authored-by: kyoninh <132638948+kyoninh@users.noreply.github.com>

---
+++ Assets/Runtime/Actors/Enemies/FoamFist/ChargeBehavior.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScringloGames.ColorClash.Runtime.Movement;
+using ScringloGames.ColorClash.Runtime.Shared.Attributes;
+using TravisRFrench.Attributes.Runtime;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime
+{
+    public class ChargeBehavior : MonoBehaviour
+    {
+          [SerializeField]
+          private AttributeBank attributeBank;
+          [SerializeField]
+          private DestinationMover mover;
+          [SerializeField]
+          private float chargeSpeed = 5f;
+          [SerializeField]
+          private float duration = 4f;
+          private float durationT;
+          private Vector2 target;
+          private AttributeModifier modifier;
+          private Vector2 previousPos;
+          void OnEnable()
+          {
+               this.modifier = new AttributeModifier()
+               {
+                    Value = chargeSpeed,
+                    Type = ModifierType.FlatMultiplicative,
+               };
+               this.attributeBank.MovementSpeedMultiplier.AddModifier(modifier);
+               this.durationT = this.duration;
+               this.target = GameObject.FindWithTag("Player").transform.position;
+               this.mover.MoveTo(target);
+               this.mover.Arrived += Arrived;
+               previousPos = this.transform.position;
+
+
+          }
+          void Update()
+          {
+               durationT -= Time.deltaTime;
+               if(durationT <= 0 )
+               {
+                    Arrived();
+               }
+               
+          }
+          void OnDisable()
+          {
+               Debug.Log("DISABLED");
+               mover.Halt();
+               mover.Arrived -= Arrived;
+               this.attributeBank.MovementSpeedMultiplier.RemoveModifier(modifier);
+          }
+          void Arrived()
+          {
+               this.enabled = false;
+          }
+
+
+    }
+}
