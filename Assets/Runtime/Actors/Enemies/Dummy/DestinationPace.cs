1970cb3c8e21f90810fc71dc8a63f36a44449972
kyoninh <132638948+kyoninh@users.noreply.github.com>
2024-03-19 20:38:07 -0700

    [FEATURE] COLOR-197 Added Foam Fist Level Sequence (#53)
    * added button prompt buttons for oboarding level sequence
    
    * added more scenes to 'OnBoardingSequence' folder
    
    * made changes to build settings
    
    * added a few levels to foamfistsequence
    
    * added additional levels to foamfistsequence
    
    * updated room list and project build settings to include added scenes
    
    * Generated missing meta files.
    
    ---------
    
    Co-authored-by: Travis French <travis.french.1991@gmail.com>

---
+++ Assets/Runtime/Actors/Enemies/Dummy/DestinationPace.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding.Util;
+using ScringloGames.ColorClash.Runtime.Movement;
+using ScringloGames.ColorClash.Runtime.Weapons;
+using UnityEngine;
+using UnityEngine.InputSystem.Android;
+
+namespace ScringloGames.ColorClash.Runtime
+{
+    public class DestinationPace : MonoBehaviour
+    {
+        [SerializeField]  
+        private DestinationMover mover;
+        [SerializeField]
+        private GameObject point1;
+        [SerializeField]
+        private GameObject point2;
+        private GameObject target;
+
+         void OnEnable()
+        {
+            mover.Arrived += Arrived;
+            if(point1 != null && point2 != null)
+            {
+                this.mover.MoveTo(point1.transform.position);
+                this.target = point1;
+            }
+        }
+        void OnDisable()
+        {
+            this.mover.Arrived -= Arrived;
+        }
+        void Arrived()
+        {
+            Debug.Log("arrived");
+            if(point1 != null && point2 != null)
+            {
+                if(target == point1)
+                {
+                    Debug.Log("flungle :)");
+                    target = point2;
+                    mover.MoveTo(point2.transform.position);
+                    Debug.Log(point2.transform.position);
+                }
+                else if (target == point2)
+                {
+                    Debug.Log("flungus :(");
+                    target = point1;
+                    mover.MoveTo(point1.transform.position);
+                }
+            }
+        }
+    }
+}
