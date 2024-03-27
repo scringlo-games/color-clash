61c6d3d1a31c1f8440eb1bd0a4223045f7d1e44f
kyoninh <132638948+kyoninh@users.noreply.github.com>
2024-03-27 14:06:06 -0700

    [FEATURE] COLOR-111 Screen Shake on Player damage.  (#58)
    * added damage vignette to main canvas
    
    * adjusted the room kit prefab to include changes
    
    * modified 'Enable on Damage' script to ignore damage below a certain threshold
    
    * added shake on enable script and enable behavior on damage script

---
+++ Assets/Runtime/UI/EnableOnDamage.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScringloGames.ColorClash.Runtime.Damage;
+using ScringloGames.ColorClash.Runtime.Shared.GameObjectFilters;
+using UnityEngine;
+
+namespace ScringloGames.ColorClash.Runtime
+{
+    public class EnableOnDamage : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject target;
+        [SerializeField]
+        private float maxDuration;
+        private float duration;
+        [SerializeField]
+        private DamageArgsEvent damagedEvent;
+        [SerializeField]
+        private GameObjectFilterSet filter;
+        void OnEnable()
+        {
+            this.duration = maxDuration;
+            damagedEvent.Raised += OnDamaged;
+        }
+        void OnDisable()
+        {   
+            damagedEvent.Raised -= OnDamaged;
+
+        }
+        void OnDamaged(DamageArgs args)
+        {
+            Debug.Log("This is Damaged");
+            if(this.filter.Evaluate(args.Receiver.gameObject) && args.Amount >= 5f)
+            {
+                StartCoroutine(EnableObject());
+            }
+        }
+        IEnumerator EnableObject()
+        {
+            target.SetActive(true);
+            yield return new WaitForSeconds(duration);
+            target.SetActive(false);
+            StopCoroutine(EnableObject());
+        }
+    }
+}
