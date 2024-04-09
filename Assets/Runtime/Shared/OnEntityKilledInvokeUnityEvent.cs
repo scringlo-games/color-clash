f807e8abac00ee6e8628c46c88942e7c6be99b29
Travis French <travis.french.1991@gmail.com>
2024-04-09 13:16:28 -0700

    Added pencil pusher boss.

---
+++ Assets/Runtime/Shared/OnEntityKilledInvokeUnityEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ScringloGames.ColorClash.Runtime.Shared
+{
+    public class OnEntityKilledInvokeUnityEvent : MonoBehaviour
+    {
+        [SerializeField]
+        private Killable killable;
+        [SerializeField]
+        private UnityEvent<Killable> response;
+
+        private void OnEnable()
+        {
+            this.killable.Killed += this.OnKilled;
+        }
+
+        private void OnDisable()
+        {
+            this.killable.Killed -= this.OnKilled;
+        }
+
+        private void OnKilled(Killable killable)
+        {
+            this.response.Invoke(killable);
+        }
+    }
+}
